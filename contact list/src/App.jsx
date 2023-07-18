defb14f63a3dd09482b62d9ac98bedbf1f106952
isaiasG-A <isaiasgarciaariza@gmail.com>
2023-07-17 19:18:06 -0700

    Single contacts can now be accessed

--- contact list/src/App.jsx
+++ contact list/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 
 import ContactList from './components/ContactList';
+import SelectedContact from './components/SelectedContact';
 
 function App() {
   const [selectedContactId, setSelectedContactId] = useState(null);
@@ -9,7 +10,7 @@ function App() {
   return (
     <>
       {
-        selectedContactId ? (<div>Selected Contact View </div>) : (<ContactList setSelectedContactId ={setSelectedContactId}/>) 
+        selectedContactId ? (< SelectedContact selectedContactId={selectedContactId} setSelectedContactId={setSelectedContactId}/>) : (<ContactList setSelectedContactId ={setSelectedContactId}/>) 
       }
     </>
   )
---
+++ contact list/src/components/SelectedContact.jsx
@@ -0,0 +1,45 @@
+import {useState, useEffect} from "react";
+
+
+export default function SelectedContact({selectedContactId, setSelectedContactId}) {
+  const [contact, setContact] = useState({});
+
+  useEffect(() => {
+
+    async function fetchContactId() {
+      try {
+        const response = await fetch(`http://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/posts/${selectedContactId}`);
+        const result = await response.json();
+        setContact(result);
+
+      } catch(error) {
+        console.error(error);
+      }
+    }
+
+    fetchContactId();
+  }, []);
+
+  return (
+    <>
+    <table>
+      <thead>
+        <tr>
+          <th colSpan="3">Contact</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Title</td>
+          <td>Body</td>
+        </tr>
+        <tr>
+         <td>{contact.title}</td>
+         <td>{contact.body}</td>
+         </tr>
+         <button onClick={() => {setSelectedContactId(null)}}>Main Page</button>
+      </tbody>
+    </table>
+    </>
+  )
+}
