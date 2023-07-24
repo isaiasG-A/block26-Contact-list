4e730f7ac82f79788b1020aed6761325a4225d36
isaiasG-A <isaiasgarciaariza@gmail.com>
2023-07-23 17:19:03 -0700

    css has been added

--- contact list/src/components/ContactRow.jsx
+++ contact list/src/components/ContactRow.jsx
@@ -3,8 +3,8 @@ import React from "react";
 export default function ContactRow({setSelectedContactId, contact}) {
   return (
     <tr onClick={() => { setSelectedContactId(contact.id)}}>
-    <td>{contact.title}</td>
-    <td>{contact.body}</td>
+    <td className="contact">{contact.title}</td>
+    <td className="contact">{contact.body}</td>
   </tr>
   )
 }
